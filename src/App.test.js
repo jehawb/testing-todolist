975f3dfdc3f0796e1c5abdbe0b8227a4fc206ec5
Ismo Harjunmaa <ismo.harjunmaa@agileo.fi>
2023-02-16 17:15:01 +0200

    Jest additions

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
+import React from "react";
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+/**
+ * @jest-environment jsdom
+ */
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  render(<App />, div);
+  expect(div).not.toBeNull();
 });
